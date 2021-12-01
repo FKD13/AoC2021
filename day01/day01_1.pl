74aca7c040b17c8e27d3a8c2277b58575c7251ba
FKD13 <francisklinck@gmail.com>
2021-12-01 23:33:27 +0100

    apply feedback

--- day01/day01_1.pl
+++ day01/day01_1.pl
@@ -1,27 +1,21 @@
 :- use_module('../util/parser').
 
+:- use_module(library(dcg/basics)).
+
 :- initialization(main).
 
-digit(N) --> [N], {member(N, [48, 49, 50, 51, 52, 53, 54, 55, 56, 57])}.
-digits([N | Ns]) --> digit(N), digits(Ns).
-digits([N]) --> digit(N).
-
-num(N) --> digits(Ns), {number_codes(N, Ns)}.
-
-nums([N | Ns]) --> num(N), "\n", nums(Ns).
-nums([N]) --> num(N).
-
-
-
+nums([N | Ns]) --> number(N), eol, nums(Ns).
+nums([N]) --> number(N), eol.
 
 process([_], []) :- !.
 process([V1, V2 | Rest], [increased | Result]) :- V1 < V2, !, process([V2 | Rest], Result).
 process([V1, V2 | Rest], [decreased | Result]) :- V1 > V2, !, process([V2 | Rest], Result).
-process([_, V2 | Rest], [equal | Result]) :- !, process([V2 | Rest], Result).
+process([_ | Rest], [equal | Result]) :- !, process(Rest, Result).
 
 main :- 
     parse('input/day01_1.txt', nums, Numbers),
     process(Numbers, Result),
-    foldl([V1, V2, Res]>>(V1 = increased -> Res is V2 + 1; Res = V2), Result, 0, Count),
+    include([X]>>(X = increased), Result, Filtered),
+    length(Filtered, Count),
     write(Count), nl,
     halt.
--- day01/day01_2.pl
+++ day01/day01_2.pl
@@ -1,28 +1,21 @@
 :- use_module('../util/parser').
 
+:- use_module(library(dcg/basics)).
+
 :- initialization(main).
 
-digit(N) --> [N], {member(N, [48, 49, 50, 51, 52, 53, 54, 55, 56, 57])}.
-digits([N | Ns]) --> digit(N), digits(Ns).
-digits([N]) --> digit(N).
-
-num(N) --> digits(Ns), {number_codes(N, Ns)}.
-
-nums([N | Ns]) --> num(N), "\n", nums(Ns).
-nums([N]) --> num(N).
-
-calculate_windows(V1, V2, V3, V4, W1, W2) :- 
-    W1 is V1 + V2 + V3,
-    W2 is V2 + V3 + V4.
+nums([N | Ns]) --> number(N), eol, nums(Ns).
+nums([N]) --> number(N), eol.
 
 process([_], []) :- !.
-process([V1, V2, V3, V4 | Rest], [increased | Result]) :- calculate_windows(V1, V2, V3, V4, W1, W2), W1 < W2, !, process([V2, V3, V4 | Rest], Result).
-process([V1, V2, V3, V4 | Rest], [decreased | Result]) :- calculate_windows(V1, V2, V3, V4, W1, W2), W1 > W2, !, process([V2, V3, V4 | Rest], Result).
+process([V1, V2, V3, V4 | Rest], [increased | Result]) :- V1 < V4, !, process([V2, V3, V4 | Rest], Result).
+process([V1, V2, V3, V4 | Rest], [decreased | Result]) :- V1 > V4, !, process([V2, V3, V4 | Rest], Result).
 process([_ | Rest], [equal | Result]) :- !, process(Rest, Result).
 
 main :- 
     parse('input/day01_1.txt', nums, Numbers),
     process(Numbers, Result),
-    foldl([V1, V2, Res]>>(V1 = increased -> Res is V2 + 1; Res = V2), Result, 0, Count),
+    include([X]>>(X = increased), Result, Filtered),
+    length(Filtered, Count),
     write(Count), nl,
     halt.
